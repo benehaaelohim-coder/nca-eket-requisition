a185eb87c32429ad1ec6db65cec624dae590b183
benehaaelohim-coder <benehaaelohim@gmail.com>
2026-09-07 14:55:54 +0100

    Update sw.js
    Update NCA EKET to Version 6.1

--- sw.js
+++ sw.js
@@ -1,5 +1,109 @@
-const CACHE='nca-eket-v1';
-const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+const CACHE = 'nca-eket-v6-1';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+
+self.addEventListener(
+  'install',
+  event => {
+
+    self.skipWaiting();
+
+    event.waitUntil(
+
+      caches
+        .open(CACHE)
+        .then(cache => {
+
+          return cache.addAll(
+            ASSETS
+          );
+
+        })
+
+    );
+
+  }
+);
+
+
+self.addEventListener(
+  'activate',
+  event => {
+
+    event.waitUntil(
+
+      caches
+        .keys()
+        .then(keys => {
+
+          return Promise.all(
+
+            keys
+              .filter(key => {
+
+                return key !== CACHE;
+
+              })
+              .map(key => {
+
+                return caches.delete(key);
+
+              })
+
+          );
+
+        })
+        .then(() => {
+
+          return self.clients.claim();
+
+        })
+
+    );
+
+  }
+);
+
+
+self.addEventListener(
+  'fetch',
+  event => {
+
+    if (
+      event.request.method !== 'GET'
+    ) {
+
+      return;
+
+    }
+
+
+    event.respondWith(
+
+      fetch(event.request)
+
+        .then(response => {
+
+          return response;
+
+        })
+
+        .catch(() => {
+
+          return caches.match(
+            event.request
+          );
+
+        })
+
+    );
+
+  }
+);
